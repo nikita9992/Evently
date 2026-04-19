60ff0bd168aa96daf841d76e69b5914074f93bde
dkjeanr <raqblacor2@alu.edu.gva.es>
2026-04-19 12:19:53 +0200

    Añadidos controladores de Pedidos, Clientes, Estados y DetallesPedido

---
+++ backend/Evently/Evently.API/Controllers/DetallePedidosController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Evently.API.Data;
+using Evently.API.Models;
+
+namespace Evently.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DetallePedidosController : ControllerBase
+    {
+        private readonly EventlyDbContext _context;
+
+        public DetallePedidosController(EventlyDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/DetallePedidos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DetallePedido>>> GetDetallesPedido()
+        {
+            return await _context.DetallesPedido.ToListAsync();
+        }
+
+        // GET: api/DetallePedidos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DetallePedido>> GetDetallePedido(int id)
+        {
+            var detallePedido = await _context.DetallesPedido.FindAsync(id);
+
+            if (detallePedido == null)
+            {
+                return NotFound();
+            }
+
+            return detallePedido;
+        }
+
+        // PUT: api/DetallePedidos/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDetallePedido(int id, DetallePedido detallePedido)
+        {
+            if (id != detallePedido.IdPedido)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(detallePedido).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DetallePedidoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/DetallePedidos
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<DetallePedido>> PostDetallePedido(DetallePedido detallePedido)
+        {
+            _context.DetallesPedido.Add(detallePedido);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (DetallePedidoExists(detallePedido.IdPedido))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetDetallePedido", new { id = detallePedido.IdPedido }, detallePedido);
+        }
+
+        // DELETE: api/DetallePedidos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDetallePedido(int id)
+        {
+            var detallePedido = await _context.DetallesPedido.FindAsync(id);
+            if (detallePedido == null)
+            {
+                return NotFound();
+            }
+
+            _context.DetallesPedido.Remove(detallePedido);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool DetallePedidoExists(int id)
+        {
+            return _context.DetallesPedido.Any(e => e.IdPedido == id);
+        }
+    }
+}
